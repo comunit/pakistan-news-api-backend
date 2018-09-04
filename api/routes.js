ef4c484245bb3aaddf6ba19b99b1f27e5a9fc0c1
Imran Rafique <comunit76@gmail.com>
2018-09-04 23:20:11 +0100

    fixed timing issues

--- api/routes.js
+++ api/routes.js
@@ -1,62 +1,65 @@
-const express = require('express');
-const topStorySave = require('../models/news-model');
-const timeSave = require('../models/time-model');
+const express = require("express");
+const topStorySave = require("../models/news-model");
+const timeSave = require("../models/time-model");
+const moment = require("moment");
 const router = express.Router();
 
 // ary news route
-router.get('/arynews', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  topStorySave.findOne({_id: 'arynews'}).then((data) => {
+router.get("/arynews", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  topStorySave.findOne({ _id: "arynews" }).then(data => {
     res.json(data.Data);
-  })
-})
+  });
+});
 
 // daily pakistan route
-router.get('/dailypakistan', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  topStorySave.findOne({_id: 'dailypakistan'}).then((data) => {
+router.get("/dailypakistan", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  topStorySave.findOne({ _id: "dailypakistan" }).then(data => {
     res.json(data.Data);
-  })
-})
+  });
+});
 
 // dunya news route
-router.get('/dunyanews', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  topStorySave.findOne({_id: 'dunyanews'}).then((data) => {
+router.get("/dunyanews", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  topStorySave.findOne({ _id: "dunyanews" }).then(data => {
     res.json(data.Data);
-  })
-})
+  });
+});
 
 // dawn news route
-router.get('/dawnnews', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  topStorySave.findOne({_id: 'dawnnews'}).then((data) => {
+router.get("/dawnnews", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  topStorySave.findOne({ _id: "dawnnews" }).then(data => {
     res.json(data.Data);
-  })
-})
+  });
+});
 
 // geo news route
-router.get('/geonews', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  topStorySave.findOne({_id: 'geonews'}).then((data) => {
+router.get("/geonews", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  topStorySave.findOne({ _id: "geonews" }).then(data => {
     res.json(data.Data);
-  })
-})
+  });
+});
 
 // express tribune route
-router.get('/expresstribune', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  topStorySave.findOne({_id: 'expresstribune'}).then((data) => {
+router.get("/expresstribune", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  topStorySave.findOne({ _id: "expresstribune" }).then(data => {
     res.json(data.Data);
-  })
-})
+  });
+});
 
 // time toute
-router.get('/time', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  timeSave.findOne({_id: 'time'}).then((data) => {
-    res.json(data.time);
-  })
-})
+router.get("/time", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  timeSave.findOne({ _id: "time" }).then(data => {
+    var time = moment.utc(data.time, "ddd MMM D YYYY HH:mm:ss ZZ");
+    var updatedMinutes = moment().diff(time, "minutes");
+    res.json(updatedMinutes);
+  });
+});
 
 module.exports = router;
